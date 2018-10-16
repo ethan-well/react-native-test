e57fe00f689f82d309c6bb4c28aacd9ff8a5c055
魏巍 <wewin11235@163.com>
2018-10-16 18:08:17 +0800

    feat(test): test textInput

--- imoocApp/ios/Test/TestComponent.js
+++ imoocApp/ios/Test/TestComponent.js
@@ -1,57 +1,20 @@
-timport React, { Component } from 'react';
-import { Alert, AppRegistry, Button, StyleSheet, View } from 'react-native';
+import React, { Component } from 'react';
+import { TextInput } from 'react-native';
 
-export default class testComponent extends Component {
-  _onPressButton() {
-    Alert.alert('You tapped the button!')
+export default class UselessTextInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: 'Useless Placeholder' };
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <View style={styles.buttonContainer}>
-          <Button
-            onPress={this._onPressButton}
-            title="Press Me"
-          />
-        </View>
-        <View style={styles.buttonContainer}>
-          <Button
-            onPress={this._onPressButton}
-            title="Press Me"
-            color="#841584"
-          />
-        </View>
-        <View style={styles.alternativeLayoutButtonContainer}>
-          <Button
-            onPress={this._onPressButton}
-            title="This looks great!"
-          />
-          <Button
-            onPress={this._onPressButton}
-            title="OK!"
-            color="#841584"
-          />
-        </View>
-      </View>
+      <TextInput
+        editable={false}
+        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={(text) => this.setState({text})}
+        value={this.state.text}
+      />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-   flex: 1,
-   justifyContent: 'center',
-  },
-  buttonContainer: {
-    margin: 20
-  },
-  alternativeLayoutButtonContainer: {
-    margin: 20,
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  }
-});
-
-// skip this line if using Create React Native App
-// AppRegistry.registerComponent('AwesomeProject', () => ButtonBasics);
